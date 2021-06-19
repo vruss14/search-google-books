82a58dd48bcade707a69531fa843e47725b3c307
Valerie Russell <vruss14@gmail.com>
2021-06-18 22:59:54 -0400

    implemented react-router-dom for navigation between views

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import SearchPage from "./pages/search.js";
 import SavePage from "./pages/save.js";
 
 function App() {
   return (
-   <SavePage />
+    <Router>
+
+      <Switch>
+
+        <Route exact path={["/", "/search"]}>
+          <SearchPage />
+        </Route>
+
+        <Route exact path="/saved">
+          <SavePage />
+        </Route>
+
+      </Switch>
+
+  </Router>
   );
 }
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,17 +3,17 @@ import React from "react";
 function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Search Google Books</a>
+            <a className="navbar-brand" href="/">Search Google Books</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarText">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Search</a>
+                        <a className="nav-link" href="/search">Search</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Saved</a>
+                        <a className="nav-link" href="/saved">Saved</a>
                     </li>
                 </ul>
             </div>
