82540ec4a664fd6f5fa768ffe9163f70e6ac367f
Valerie Russell <vruss14@gmail.com>
2021-06-18 21:48:58 -0400

    set up MongoDB and installed Mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +19,9 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+{ useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> Express/Node.js API server now on http://localhost:${PORT}`);
