2e0e3faf603c2de9e7acd9bfdbbab2a705f92df0
Valerie Russell <vruss14@gmail.com>
2021-07-15 16:13:51 -0400

    updated book model so that books without images/authors can be saved

--- models/book.js
+++ models/book.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  authors: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
+  authors: { type: String },
+  description: { type: String },
+  image: { type: String },
   link: { type: String, required: true },
 });
 
